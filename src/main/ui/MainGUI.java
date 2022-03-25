713701092a8a42d4c25ed2b179f24adcdd40df5b
sahibrao <sahibrao15@gmail.com>
2022-03-25 14:35:18 -0700

    added class level comments

--- src/main/ui/MainGUI.java
+++ src/main/ui/MainGUI.java
@@ -1,6 +1,5 @@
 package ui;
 
-import model.Americano;
 import model.Drink;
 import model.Order;
 import persistance.JsonReader;
@@ -12,7 +11,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.SQLOutput;
 
 // cited from AlarmSystem - AlarmControllerUI.java
 public class MainGUI extends JFrame {
@@ -53,17 +51,16 @@ public class MainGUI extends JFrame {
         controlPanel.setVisible(true);
         desktop.add(controlPanel);
 
-
         validate();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         centreOnScreen();
         setVisible(true);
     }
 
+    // EFFECTS: starts up the mainGUI
     public static void main(String[] args) {
         new MainGUI();
     }
-
 
     // MODIFIES: this
     // EFFECTS: a helper function to aid in setting up a button for each function
@@ -92,10 +89,13 @@ public class MainGUI extends JFrame {
     // EFFECTS: adds a coffee based on user's input
     private class AddCoffee extends AbstractAction {
 
+        // EFFECTS: constructor for AddCoffee()
         AddCoffee() {
             super("Order Coffee");
         }
 
+        // EFFECTS: creates a panel of asking which coffee they'd
+        // like and receiving input to put in orderingCoffee method
         @Override
         public void actionPerformed(ActionEvent evt) {
             String coffeeOrdered = JOptionPane.showInputDialog(null,
@@ -106,6 +106,9 @@ public class MainGUI extends JFrame {
             orderingCoffee(coffeeOrdered);
         }
 
+        // REQUIRES: String coffeeOrdered has to be a drink and a size separated by a space button
+        // MODIFIES: this
+        // EFFECTS: adds a coffee to order based on String coffeeOrdered
         private void orderingCoffee(String coffeeOrdered) {
             int spaceIndex = coffeeOrdered.indexOf(" ");
             switch (coffeeOrdered.substring(0, spaceIndex)) {
@@ -133,10 +136,14 @@ public class MainGUI extends JFrame {
     // EFFECTS: removes a coffee based on user's input
     private class RemoveCoffee extends AbstractAction {
 
+        // EFFECTS: constructor for RemoveCoffee()
         RemoveCoffee() {
             super("Remove Coffee");
         }
 
+        // MODIFIES: this
+        // EFFECTS: creates a panel of asking which coffee they'd
+        // like to remove and removes it from order
         @Override
         public void actionPerformed(ActionEvent evt) {
             String removeCoffee = JOptionPane.showInputDialog(null,
@@ -151,38 +158,53 @@ public class MainGUI extends JFrame {
     // EFFECTS: releases a summary of all the drinks so far and the prices
     private class Summary extends AbstractAction {
 
+        // EFFECTS: constructor for Summary()
         Summary() {
             super("Coffee Summary");
         }
 
+        // EFFECTS: creates a summary of all the drinks as well as add a picture based on how many drinks are ordered
         @Override
         public void actionPerformed(ActionEvent evt) {
             String message;
             message = "Here is the list of drinks:\n";
+            ImageIcon icon1 = new ImageIcon("./data/coffee1.png");
+            ImageIcon icon2 = new ImageIcon("./data/coffee2.jpeg");
+            ImageIcon icon3 = new ImageIcon("./data/coffee3.jpeg");
             for (Drink d : order.getList()) {
                 message += "-" + d.getNameDrink() + " = $" + String.format("%.2f", d.getPrice()) + "\n";
             }
-            JOptionPane.showMessageDialog(null, message, "Coffee Summary",
-                    JOptionPane.PLAIN_MESSAGE);
 
-            JLabel label = new JLabel();
-            label.setIcon(new ImageIcon("/data/coffee1.png"));
-            label.setBounds(100, 100, 100, 100);
-            JPanel panel = new JPanel();
-            panel.add(label);
-            panel.setVisible(true);
-            add(panel);
+            if (1 == order.getList().size()) {
+                JOptionPane.showMessageDialog(null, message, "Coffee Summary",
+                        JOptionPane.PLAIN_MESSAGE, icon1);
+            } else if (2 == order.getList().size()) {
+                JOptionPane.showMessageDialog(null, message, "Coffee Summary",
+                        JOptionPane.PLAIN_MESSAGE, icon2);
+            } else if (0 == order.getList().size()) {
+                JOptionPane.showMessageDialog(null, message, "Coffee Summary",
+                        JOptionPane.PLAIN_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, message, "Coffee Summary",
+                        JOptionPane.PLAIN_MESSAGE, icon3);
+            }
+
 
         }
     }
 
+    // MODIFIES: order.JSON
     // EFFECTS: loads order from file order.JSON
     public class LoadOrder extends AbstractAction {
 
+        // EFFECTS: constructor for LoadOrder()
         LoadOrder() {
             super("Load Order");
         }
 
+        // REQUIRES: order.JSON should be readable
+        // MODIFIES: order.JSON
+        // EFFECTS: loads the order from order.JSON
         @Override
         public void actionPerformed(ActionEvent evt) {
             try {
@@ -201,10 +223,14 @@ public class MainGUI extends JFrame {
     // EFFECTS: saves order from file order.JSON
     public class SaveOrder extends AbstractAction {
 
+        // EFFECTS: constructor for SaveOrder()
         SaveOrder() {
             super("Save Order");
+
         }
 
+        // REQUIRES: order.JSON to be writable
+        // EFFECTS: saves order into file order.JSON
         @Override
         public void actionPerformed(ActionEvent evt) {
             try {
@@ -226,10 +252,13 @@ public class MainGUI extends JFrame {
     // EFFECTS: gives the options in customizing a drink
     public class CustomizeCoffee extends AbstractAction {
 
+        // EFFECTS: constructor for CustomizeCoffee()
         CustomizeCoffee() {
             super("Customize Coffee");
         }
 
+        // EFFECTS: creates a panel of asking which coffee they'd
+        // like to customize and put in customizeDrink method
         @Override
         public void actionPerformed(ActionEvent evt) {
             String message = "Enter number of drink customized, followed by \n";
@@ -244,6 +273,9 @@ public class MainGUI extends JFrame {
 
         }
 
+        // REQUIRES: String customizeCoffee0 has to be separated by "," for each customization
+        // MODIFIES: this
+        // EFFECTS: customizes a coffee to order based on String customizeCoffee0
         private void customizeDrink(String customizeCoffee0) {
             int index0 = customizeCoffee0.indexOf(",");
             String numDrink = customizeCoffee0.substring(0, index0);
@@ -259,9 +291,8 @@ public class MainGUI extends JFrame {
             String customizeCoffee4 = customizeCoffee3.substring(index3 + 1);
             int index4 = customizeCoffee4.indexOf(",");
             String caramel = customizeCoffee4.substring(0, index4);
-            String customizeCoffee5 = customizeCoffee4.substring(index4 + 1);
-            int index5 = customizeCoffee5.indexOf(",");
-            String cinnamon = customizeCoffee5;
+            String cinnamon = customizeCoffee4.substring(index4 + 1);
+
 
             order.getList().get(Integer.parseInt(numDrink) - 1).addMilk(milk);
             order.getList().get(Integer.parseInt(numDrink) - 1).addSugar(Integer.parseInt(sugar));
@@ -270,5 +301,4 @@ public class MainGUI extends JFrame {
         }
     }
 
-    // need a class to make pictures of coffee appear
 }
